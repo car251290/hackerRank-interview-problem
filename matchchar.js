529e4ac062b042522b79526db1421591cbefea28
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2023-10-15 17:26:16 -0700

    Add files via upload

---
+++ matchchar.js
@@ -0,0 +1,72 @@
+
+function maxChar(str) {
+
+        const charMap = {};
+        let max = 0;
+        let maxChar = '';
+
+        for(let char of str){
+            if(charMap[char]){
+                charMap[char]++;
+            }else{
+                charMap[char] = 1;
+            }
+        }
+
+        for(let char in charMap){
+            if(charMap[char] > max){
+                max = charMap[char];
+                maxChar = char;
+            }
+        }
+
+        return maxChar; 
+}
+
+
+function maxChar (str) {
+
+    const charMap = {}
+    let max = 0;
+    let maxChar ="";
+
+    for(let char of str){
+        if(charMap[char]){
+            charMap[char]++;
+        } else {
+            charMap[char]=1;
+        }
+    }
+
+    for (let char in charMap){
+        if(charMap[char]> max){
+            max = charMap[char];
+            maxChar = char
+        }
+    }
+    return maxChar
+
+}
+
+function maxchar () {
+const charMap = {};
+let maxChar = "";
+let max = []
+
+for(let char in charMap){
+    if(charMap[char]){
+        charMap[char]++;
+    } else {
+        charMap[char] = 1
+    }
+
+}
+for (let char in charMap) {
+    if(charMap[char]> max){
+        max = charMap[char]
+        maxChar = char
+    }
+}
+return maxChar;
+}
+
