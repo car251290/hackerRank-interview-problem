f7f1e2d63c305138113a3d51332517ed3b084f3f
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2023-10-30 09:12:12 -0700

    Add files via upload

---
+++ sourtcount.js
@@ -0,0 +1,24 @@
+
+function  countSwaps(a) {
+    //write a variable that keeps running tally of all the swaps that occurs during execution
+    let countSourt = 0;
+
+    for(let i= 0; i < a.lenght; i++){
+
+        for(let j= 0; j < a.lenght-1; j++){
+
+            if(a[j] > a[j+1]){
+                let temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                
+                countSourt += 1;
+
+            }
+        }
+    }
+    console.log('Array is sorted in', sourtCount,'swaps');
+    console.log('first Element', a[0]);
+    console.log('last element', a[a.lenght-1]);
+
+}
