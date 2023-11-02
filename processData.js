6731fafc7a7dd744e148cc2029bba92eff729e68
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2023-11-01 20:34:52 -0700

    Add files via upload

--- processData.js
+++ processData.js
@@ -1,48 +1,57 @@
-function processData(input) {
-
-function obj (name,score) {
-  name = this.name;
-  score = this.score;
-   }
-   //instantion of the name and score
-const myobj = new Obj("name",score);
-   
-
-   // input.sort(( a, b) => a - b);
-function comparator(a,b) {
-    //compare two objects
-     // compare the score with an if statment 
-    if(a.score > b.score){
-        return -1
-    } if (a.score < b.score){
-        return 1
-    } if(a.name < b.name){
-        return -1
-    } if(a.name > b.name){
-        return 1
-    } 
-    return 0
-
-};
-    //sort the arrays
-    n = int(input());
-    data = [];
-    for(let i = 0; i < n.length; i++) {
-
-        name, score = input().split();
-        score = int(score);
-        obj = Obj(name,score)
-        data.append(obj) 
+//class player to make the comparation 
+class Player {
+    constructor(name,score) {
+        name = this.name;
+        score = this.score;
     }
-data = sorted(data,key=processData(Obj.comparator))
-for(let i= 0; data.length;i++){
-    print(i.name,i.score);
 }
+    function processData(input) {
 
-    //const Score = input.forEach( input => {
+        //function obj (name,score) {
+          //name = this.name;
+          //score = this.score;
+           //}
+           //instantion of the name and score
+        const myobj = new Obj("name",score);
+           
+        
+           // input.sort(( a, b) => a - b);
+           //make a function for the comparation
+        function comparator(a,b) {
+            //compare two objects
+             // compare the score with an if statment 
+            if(a.score > b.score){
+                return -1
+            } if (a.score < b.score){
+                return 1
+            } if(a.name < b.name){
+                return -1
+            } if(a.name > b.name){
+                return 1
+            } 
+            return 0
+        
+        };
+            //sort the arrays
+            n = int(input());
+            data = [];
+            for(let i = 0; i < n.length; i++) {
+        
+                name, score = input().split();
+                score = int(score);
+                obj = Obj(name,score)
+                data.append(obj) 
+            }
+        data = sorted(data,key=processData(Obj.comparator))
+        for(let i= 0; data.length;i++){
+            print(i.name,i.score);
+        }
+    }
 
-    //})
 
-}
+
+
 
 //https://forum.freecodecamp.org/t/help-needed-with-a-problem-solved-hackerrank-day-6/396355/3
+
+//https://medium.com/voice-of-code/sorting-comparator-21d98c192bbb
