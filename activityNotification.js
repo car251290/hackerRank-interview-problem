b5c13b207f8cdb43071221934ccd03f4bb188685
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2023-12-11 09:29:49 -0800

    Add files via upload

--- activityNotification.js
+++ activityNotification.js
@@ -18,5 +18,55 @@ function activityNotifications(expenditure,d){
 
 
     //days 3 
-    
+ let Range = 201;
+ let notification = 0
+ const getDoubleMedian = (count,d) => {
+    let countPrefix = Array.from(count)
+    for(let i =0; i< count.length; i++){
+        countPrefix[i] += countPrefix[i-1] || 0
+    }
+    let a = 0;
+    let b = 0;
+// only need the middle elements , so skip build the array to avoid TLE
+if(d % 2 === 0){
+    let first = Math.floor(d/2);
+    let second = first + 1
+    for(let i = 0; i<Range; i++){
+        if(first <= countPrefix[i]){
+            a = i;
+            break;
+        }
+
+    } 
+    //forloop of the rage to get it.
+    for(let i =0; i< Range; i++){
+        if(second <= countPrefix[i]){
+            b[i]
+            break
+        }
+    }
+} else {
+    let first = Math.floor(d/2)+1
+    for(let i=0; i<Range; i++){
+        if(first <= countPrefix[i]){
+            a= i;
+            b=i;
+            break
+        }
+    }
 }
+return a +b
+ }
+    // get the range with the notification
+    const count = Array(Range).fill(0)
+    for(let i = d; i < expenditure.length; i++) {
+        const median = getDoubleMedian(count, d)
+            if(expenditure[i] >= median){
+                notification++
+            }
+            // update the count table
+            count[expenditure[i]] ++
+            count[expenditure[i - d]]--
+        }
+        return notification
+    }
