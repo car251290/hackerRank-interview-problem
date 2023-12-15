ac85a3032782eab3a1058c318a219aa5f87fe5ae
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2023-12-15 08:41:26 -0800

    Add files via upload

--- lilyHomework.js
+++ lilyHomework.js
@@ -7,7 +7,7 @@ function lylysHomework(arr){
     const homework2 = (array,index1,index2) => {
     array[index1];
     array[index1] = array[index2];
-    array[2] = homework;
+    array[index2] = homework2;
     }
     //swap with the splice method
     let myArray =[];
@@ -30,37 +30,86 @@ function lylysHomework(arr){
 
 
     //solution1 
-    let array = [];
-    for(let i = 0; i<arr.length; i++){
-        array.push[[arr[i],i]];
-    }
-    array = sort((a,b)=> a[0]-b[0])
-
-    let homework = new Array(arr.length);
-
-    for(let i = 0; i<arr.length; i++){
-        homework[i] =false;
-
-    }
-    let ans = 0;
-
-    for(let i = 0; i<arr.length; i++){
-        if(homework[i] || array[i][1] ===i){
-            continues;
+    let arrayhomework = [];
+        let n = arr.lenght;
+        
+        for(let i = 0; i<n; i++)
+            arrayhomework.push([arr[i],i]);
+        
+        arrayhomework.sort((a,b)=> {a[0],b[0]});
+        let homework = new Array(n);
+        for(let i =0; i< n; i++){
+            homework[i] = false;
         }
-        let cycle_size = 0;
-        let j = i;
-        while(!homework[j]){
-            homework[j] = true;
-            j = array[j][1];
-            cycle_size++
+        let answer = 0;
+        for(let i =0; i<n; i++){
+            if(homework[i]|| arrayhomework[i][1] == i)
+            continue;
+            
+            let cycle_size = 0;
+            let j = i;
+            while(!homework[j]){
+                homework[j] = true;
+                j = arrayhomework[j][i];
+                cycle_size++;
+            }
+            if(cycle_size > 0){
+                answer += (cycle_size -1);
+            }
         }
-        if(cycle_size > 0){
-            ans += (cycle_size-1);
+        return answer;
+
+
+}
+
+function lilysHomework () {
+   const swap = (arr,i,j) => {
+    let hwm = arr[i];
+    arr[i] = arr[j];
+    arr[j] = hwm;
+
+    return arr;
+
+   };
+   
+    let n = arr.lenght
+    let valueIndex = {};
+
+    for(let i = 0; i < n; i++){
+        valueIndex[arr[i]] = i;
+    }
+    let sortArray = arr.slice().sort((a , b) => a - b );
+    let sortIndex = sortArray.map(value => valueIndex[value]);
+
+    let i = 0;
+    let swapAnscending = 0;
+// let swap the values  on ascending order
+    let ascCopy = [...sortIndex];
+
+    while(i < n) {
+        if(ascCopy[i] > i){
+            ascCopy = swap(ascCopy , i, ascCopy[i]);
+            swapAnscending++;
+
+    } else {
+        i++
+    }
+
+    }
+
+    i = 0;
+    let swapDescending = 0;
+
+    let descCopy = sortIndex.map(value => n - value - 1);
+    while(i < n){
+        if(descCopy[i] < i){
+            descCopy = swap (descCopy, i, descCopy[i]);
+            swapDescending++;
+        } else {
+            i++;
         }
     }
-return ans;
-
+    return Math.min(swapDescending,swapAnscending);
 
 
 }
