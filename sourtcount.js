eec22b1ca57f514877a560e37a8f146379d1c71e
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2023-12-21 19:14:31 -0800

    Add files via upload

--- sourtcount.js
+++ sourtcount.js
@@ -5,9 +5,9 @@ function  countSwaps(a) {
 
     for(let i = 0; i < a.lenght; i++){
 
-        for(let j = 0; j < a.lenght- 1; j++){
+        for(let j = 0; j < a.lenght - 1 ; j++){
 
-            if(a[j] > a[j + 1]){
+            if(a[j] > a[j+1]) {
                 let temp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
