c43753dab2943e93540c7e9ae1f10048a28e04fe
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-01-17 18:17:45 -0800

    Add files via upload

--- processData.js
+++ processData.js
@@ -1,57 +1,44 @@
-//class player to make the comparation 
-class Player {
-    constructor(name,score) {
-        name = this.name;
-        score = this.score;
-    }
-}
-    function processData(input) {
 
-        //function obj (name,score) {
-          //name = this.name;
-          //score = this.score;
-           //}
-           //instantion of the name and score
-        const myobj = new Obj("name",score);
-           
-        
-           // input.sort(( a, b) => a - b);
-           //make a function for the comparation
-        function comparator(a,b) {
-            //compare two objects
-             // compare the score with an if statment 
-            if(a.score > b.score){
-                return -1
-            } if (a.score < b.score){
-                return 1
-            } if(a.name < b.name){
-                return -1
-            } if(a.name > b.name){
-                return 1
-            } 
-            return 0
-        
-        };
-            //sort the arrays
-            n = int(input());
-            data = [];
-            for(let i = 0; i < n.length; i++) {
-        
-                name, score = input().split();
-                score = int(score);
-                obj = Obj(name,score)
-                data.append(obj) 
-            }
-        data = sorted(data,key=processData(Obj.comparator))
-        for(let i= 0; data.length;i++){
-            print(i.name,i.score);
+function processData(input) {
+     //Enter your code here
+    // to comparation the player will have two fields, name and score
+    //create a checher class that implements the comparator interface then write an int playera,playerb 
+    // sort the variables in descending order
+    
+    
+    class player {
+        constructor(name,score){
+            this.name = name;
+            this.score = score;
         }
-    }
+          // check the class that implements comparator methods
+          comparator(playerA,playerB){
+              if(playerA.score == playerB){
+                  if(playerA.name >playerB.name){
+                      return -1
+                  } else {
+                      return 1
+                  }
+              } if(playerA.score > playerB.score){
+                  return -1
+              } else {
+                  return 1
+              }
+          }
+       
+     }
+    
+    
+     let data = [];
+ // sort the variables in descending order
+     const sort = ((a,b)=> b - a);
+     sort = data.slip();
+     for(let i = 0; i<input; i++){
+        playerA;
+        playerA = name
+        playerA.score = score;
+        player.push(playerA);
 
 
-
-
-
-//https://forum.freecodecamp.org/t/help-needed-with-a-problem-solved-hackerrank-day-6/396355/3
-
-//https://medium.com/voice-of-code/sorting-comparator-21d98c192bbb
+     }
+}
