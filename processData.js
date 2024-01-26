f3a36f38ca61c5f2132dca2705fa84aae019cd0e
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-01-26 12:44:31 -0800

    Add files via upload

--- processData.js
+++ processData.js
@@ -4,7 +4,10 @@ function processData(input) {
     // to comparation the player will have two fields, name and score
     //create a checher class that implements the comparator interface then write an int playera,playerb 
     // sort the variables in descending order
-    
+        //Enter your code here
+    // to comparation the player will have two fields, name and score
+    //create a checher class that implements the comparator interface then write an int playera,playerb 
+    // sort the variables in descending order
     
     class player {
         constructor(name,score){
@@ -14,7 +17,7 @@ function processData(input) {
           // check the class that implements comparator methods
           comparator(playerA,playerB){
               if(playerA.score == playerB){
-                  if(playerA.name >playerB.name){
+                  if(playerA.name > playerB.name){
                       return -1
                   } else {
                       return 1
@@ -28,17 +31,44 @@ function processData(input) {
        
      }
     
-    
-     let data = [];
+ let data = [];
  // sort the variables in descending order
-     const sort = ((a,b)=> b - a);
-     sort = data.slip();
-     for(let i = 0; i<input; i++){
-        playerA;
-        playerA = name
+     const sort = ((a,b)=> b.score - a.score);
+     sort = data.slice(1);
+     for(let i = 0; i < input; i++){
+       // playerA;
+        //playerA = name
         playerA.score = score;
         player.push(playerA);
 
-
      }
 }
+function processData2(input) {
+    class Player {
+        constructor(name, score){
+            this.name = name;
+            this.score = score;
+            
+        }
+     }
+    console.log(Player.name, Player.score)
+    
+    //Enter your code here
+    let lines = input.split("\n")
+    let sorted = lines.splice(1)
+    .map((str) => {
+        let split = str.split(" ") 
+        return {
+            name:split[0],
+            score : parseInt(split[1])
+            
+        }
+    }).sort((a,b) => {
+        if(a.score !== b.score){
+            return b.score - a.score;
+        }
+        return a.name.localeCompare(b.name)
+    }) .map(player => `${player.name} ${player.score}`).join("\n");
+    console.log(sorted)
+    
+    };
