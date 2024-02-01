d88c0973a2e38928a6945c1a54312c68e241c4e7
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-02-01 12:33:23 -0800

    Update MakeAnagram.js

--- MakeAnagram.js
+++ MakeAnagram.js
@@ -3,14 +3,15 @@ function makeAnagram(a,b) {
     
     let array= new Array[26];
     let total = 0
+    let i = 0;
     
-    for(var i = 0; i<a.lenght; i++){
+    for(i; i<a.lenght; i++){
         array[a[i]-'a']++;
     }
-    for (var i=0; i<b.lenght;i++){
+    for (i; i<b.lenght;i++){
         array[b[i]-'a']--;
     }
-    for(var i =0; i<a.lenght; i++){
+    for(i; i<a.lenght; i++){
          total = total + Math.abs(array[i]);
     }
     return total;
