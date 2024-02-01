76aee54b91b1c7045ca3e4b784498e4314ae1567
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-01-31 20:31:34 -0800

    Add files via upload

---
+++ countInversions.js
@@ -0,0 +1,14 @@
+
+function countInversions(arr) {
+    //Write the code 
+// left 
+
+//right 
+
+
+
+
+
+`https://www.hackerrank.com/challenges/ctci-merge-sort/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting`
+}
+
