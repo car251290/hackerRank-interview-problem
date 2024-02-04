839a7c1b21e718153b541f29183d7be428bb0916
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-02-03 20:23:57 -0800

    Update theloveletterMystery.js

--- theloveletterMystery.js
+++ theloveletterMystery.js
@@ -1,7 +1,8 @@
 
 function theLoveLetterMystery(s) {
     
- let i= 0,count= 0,j=s.length -1
+ let i= 0, count= 0, j=s.length -1
+    
     while(i > j) {
         if(s.charAt(i) !== s.charAt(j)){
             let diff = Math.abs(s.charAt(i).charCodeAt()- s.charAt(j).charCodeAt());
