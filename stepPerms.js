b766377ac414c31ae90119d110e33b815b959a7f
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-02-20 16:34:14 -0800

    Add files via upload

---
+++ stepPerms.js
@@ -0,0 +1,16 @@
+function stepPerms(n){
+   `https://www.hackerrank.com/challenges/ctci-recursive-staircase/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=recursion-backtracking`
+   //loop into the right and left 
+   const memo = {
+      1:1,
+      2:2,
+
+   }
+   const result  =  memo [n];
+   
+
+// Start a range and have an end rage
+
+// if statement
+
+}
