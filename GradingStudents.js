f601df027b6322c7bcc07276e430a1f6a17e119b
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-02-24 20:02:35 -0800

    Add files via upload

---
+++ GradingStudents.js
@@ -0,0 +1,22 @@
+
+function gradingStudents(grades) {
+    // Write your code here
+    // create a variable equal 86
+    let grades = 86;
+    // will be the next method
+    let nexGrade = Math.ceil(grades/5) * 5
+    let difference = nexGrade - grades;
+
+    if (difference > 3 && grade >= 38){
+        let newGrade = Math.ceil(grades/5)*5
+        return newGrade; 
+
+
+    } else {
+        console.log(grade);
+    }
+
+    `https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true`
+    
+
+}
