6fe8b73b99196857e6a1e7ac2e8db1495092aba8
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-03-05 12:38:05 -0800

    Add files via upload

--- repeatedString.js
+++ repeatedString.js
@@ -1,6 +1,38 @@
+// solution 1
+// output is aba
+// s = 'abcac' first 10 characters of the string
+// join the arr with the string
 
+// write a counts the number of times a character is incluided in String
+function countAs(str,line){
+    let count = 0;
+    for (let i =0; i < line; i++){
+    // we need to get the if count == 'a'
+    if(str[i] === 'a'){
+        // get ingrease the count for the space on the string
+        count ++
+    }
+    }
+    // return the count of the spaces
+    return count;
+}
 function repeatedString(s, n) {
     // Write your code here
     
+return(
+    // parseInt get an interger and past back an stringe.
+    countAs(s,s.length) * parseInt(n/s.length, 10) + countAs(s,n % s.length));
 
+
+}
+
+// solution 2
+function repeatedString(){
+    let letter = (s.split("a").length - 1);
+    let max = Math.floor(n/s.length) * letter;
+    let totalAs = letter * max;
+    // cut some strings
+    let remStrings = n % s.length;
+    totalAs += (s.slice(0,remStrings).split("a").length-1)
+    return totalAs;
 }
