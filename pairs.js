2b305ab16bc8a66017b4d3fac4d5cc6317cace01
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-03-07 19:10:24 -0800

    Add files via upload

---
+++ pairs.js
@@ -0,0 +1,22 @@
+function pairs(k,arr){
+     // Write your code here
+    //get the numbers of pairs of an array elements that have equal target value
+    `https://www.hackerrank.com/challenges/pairs/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=search`
+    // container a separed spaces with intergers of the array
+    // the second one container has 2 spaces intergers n and k size of the arr
+    
+    // Make two helper one right and left
+    arr = new Array
+    // check to right and the left 
+    // then punch the current ++ to the new array
+    
+    
+    for(let i =0; i < k.length; i++ ){
+        k[i] > lower;
+    for(let j = 0; j< k.length; j--) {
+        k[j] < higher;
+    }
+    
+    } 
+    
+}
