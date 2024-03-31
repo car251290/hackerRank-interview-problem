256feea805c9e81a937115f28fa346bed5cd515d
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-03-30 19:32:42 -0700

    Add files via upload

--- jumpingOnClouds.js
+++ jumpingOnClouds.js
@@ -1,4 +1,5 @@
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c, k) {
+`https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign`
 
-function jumpingOnClouds(c){
-   `https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=7-day-campaign` 
 }
