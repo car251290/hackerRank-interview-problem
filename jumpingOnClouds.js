15410280438063b4a37f746dc8858bf97b120af0
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-04-02 14:07:53 -0700

    Add files via upload

--- jumpingOnClouds.js
+++ jumpingOnClouds.js
@@ -1,5 +1,23 @@
-// Complete the jumpingOnClouds function below.
-function jumpingOnClouds(c, k) {
-`https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign`
 
+function jumpingOnClouds(c){
+  //the minimum number of jumps 
+  //The player must avoid  and . The game can be won with a minimum of  jumps:
+  // firsth path takes 3 jumps while the second takes 4 and return 3
+  // print the minimum number of jumps needed to win the game
+  // if the array is = 0 is a safe move
+  // if the array is = 1 is not a safe move
+
+// it has an array [0,0,1,0,0,1,0]
+// jump1 and jump2 to get track of the jumps into the array 
+let jumps = 0;
+let count = c.length - 1
+
+for(let i =0; i < count; i++, jumps++){
+    if(i + 2 < c.length && c[i+2] == 0){
+        i++
+    }
 }
+return jumps;
+ 
+}
+
