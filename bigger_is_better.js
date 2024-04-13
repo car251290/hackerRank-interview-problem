4debefe1df53b0d4efcada5d0e3ef622d17ce4e6
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-04-12 17:22:28 -0700

    Add files via upload

---
+++ bigger_is_better.js
@@ -0,0 +1,14 @@
+
+`https://www.hackerrank.com/challenges/bigger-is-greater/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign`
+
+function biggerIsGreater(w) {
+    // Write your code here
+    
+
+}
+
+
+
+
+
+
