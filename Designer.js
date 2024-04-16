0c68e57b05032acfb98fd05df0b8dd37ac71436e
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-04-15 19:59:44 -0700

    Add files via upload

---
+++ Designer.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// Designer PDF 
+
+function designerPdfViewer(h,word){
+    // a is index 0 and z is index 25
+// assuming all the letter are 1 mm 
+
+
+h = [26];
+// method to [a-z]
+// the second line container is for lowercase 
+
+
+// the word abc and the a=1,b=3,c=1
+// so it will be 3 * 1 mm and will be 9 mm 
+
+
+
+
+//https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+
+
+
+
+
+}
