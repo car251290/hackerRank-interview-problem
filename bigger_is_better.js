b16837d37d44ce3218bcb6e68828a41a172fa5ce
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-04-18 12:47:51 -0700

    Add files via upload

--- bigger_is_better.js
+++ bigger_is_better.js
@@ -1,6 +1,8 @@
 
 `https://www.hackerrank.com/challenges/bigger-is-greater/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign`
 
+
+'use strict'
 function biggerIsGreater(w) {
     // Write your code here
     // swap a new wordcreate a new one
@@ -8,11 +10,12 @@ function biggerIsGreater(w) {
     // it must be the smallest word that meets the first condition
     // the word is abdc 
     // if not possible return no
+    let arrchart,n,endindex
 
-    w = [];
-    arrchart = w.split()
-    n = arrchart.length;
-    endindex = 0;
+  w = [];
+  arrchart = w.split('');
+  n = arrchart.length;
+  endindex = 0;
     // start from the right most character and find the first character that is smaller than previous character in this case it is "d" as it is smaller than "i"
     //const sort =(a,b) => b - a;
   // Start from the right most character and find the first character
@@ -27,7 +30,7 @@ function biggerIsGreater(w) {
      if(endindex == 0){
         console.log("it not answer")
      } else {
-        let firstSmallchar = arrchart[endArray - 1]
+        let firstSmallchar = arrchart[endindex - 1]
         let nextSmaller = endindex;
         for(let startIndex =  endindex + 1; startIndex > endindex; startIndex++){
             if(arrchart[startIndex] > arrchart[endArray]){
@@ -36,12 +39,19 @@ function biggerIsGreater(w) {
             }
 
         }
-        swap(arrchart,endArray-1,nextSmaller)
+       swapt(arrchart,endArray-1,nextSmaller)
          //Sort the charArray after (endIndex - 1)in ascending order
          Array.sort(arrchart,endArray)
      }
      return new String(arrchart);
     }  
+    
+    const swapt = (arrchart,i,j) => {
+      let temp = arrchart[i];
+      arrchart[i] =arrchart[j]
+      arrchart[j] = temp;
+    }
+
      //for(let j = 1; j < w.length - 1; j++){
             // it will swap the code!! 
       //      let swap = sort[j];
