d2b05c8de1438faca51ff7e15a5e49746a8e8004
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-05-24 16:02:22 -0700

    Add files via upload

---
+++ flatlandSpaceStation.js
@@ -0,0 +1,22 @@
+
+'use strict'
+`https://www.hackerrank.com/challenges/flatland-space-stations/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign`
+function flatlandSpaceStations(n, c) {
+   // The distance to the nearest space station for each city is listed below:has distance , as it contains a space station.
+   // has distance  to the space station in .
+   // has distance  to the space stations in  and .
+   // has distance  to the space station in .
+   // has distance , as it contains a space station.
+   c=["city1","city2","city3"]
+   for(let i = 0; i < c.length; i++){
+    city = c[i];
+
+   }
+   console.log(city)
+
+}
+
+
+
+
+
