a56fe2dc8d1b07a818a5a9e65ef8b5f6fd3afeec
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-06-04 16:44:33 -0700

    Add files via upload

--- day_of_Programmer.js
+++ day_of_Programmer.js
@@ -1,5 +1,4 @@
-`https://www.hackerrank.com/challenges/day-of-the-programmer/problem`
-'use strict';
+"use strict";
 function dayOfProgrammer(year){
     //write code for this challenge
     //let years = 1918 // they change the calendar 32nd day of the year in Russia.
@@ -7,37 +6,36 @@ function dayOfProgrammer(year){
     //and 28 days during a non-leap year.
     // 256th day of that year according to the official Russian calendar during that year. 
     //Then print it in the format dd.mm.yyyy, where dd is the two-digit day, mm is the two-digit month, and yyyy is .
-    if(year === 1918){
-        // 1918 is not a leap year
-        // 32nd day of the year
-        // year = 1918
-        return `26.09.1918`
-        // if it is a leap year it has to be 12.09.1984
-        // if it is not a leap year it has to be 13.09.1985
+switch(true){
+    // case one if julia was born in 1700s and is a leap year
+    // it is a leap year because is divisble by 400 and 4 and 100. 
+    case year < 1918:
+        if(year % 4 === 0){
+            // leap year it has 29 days and the year will be 
+            return `12.09.${year}`;
+        }else{
+            // non leap year it has 28 days 
+            return `13.09.${year}`;
+        }
+        // case two if julia was born in 1918 and is a leap year.
+        // 1918 is not a leap year because it is not divisible by 400 and 4
+    case year > 1918:
+        // leap year it has 29 days 
+        // 1918 is not a leap year because it is not a divisible by 400 and 4 and 100.
+        if(year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)){
+            return `12.09.${year}`;
+        }else{
+            // non leap year it has 28 days
+            return `13.09.${year}`;
+        }
+        // case 3 if julia was born in 1918 and is a leap year.
 
-    } else if(year < 1918){
-        return `12.09.1984`
-    } else if(year% 4 ===0){
-            //leap year 
-            return `12.09.1984`
-            // this one is not a leap year
-        }else if(year%4 !==0){
-            // year = 1985
-            return `13.09.1985`
-        } else if (year%100 ===0){
-            // year = 1900
-            return `13.09.1900`
-        } else if(year % 400 === 0){
-            // year = 2000
-            return `12.09.2000`
-    } else if(year > 1918){
-        return `13.09.1918`
-    } else if(year === 1918){
-        return `26.09.1918`
-    } else if(year === 2017){
-        return `13.09.2017`
-    } else if(year === 2016){
-        return `12.09.2016`
-    }
+    case year === 1918:
+        // 1918 is not a leap year because it is not a divisible by 400 and 4
+        return `26.09.${year}`;
+        // otherwise these cases are invalidad years.
+    default:
+        return `Invalid year`;
+}
     
 }
