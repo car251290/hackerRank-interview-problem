4bce8192c8720d1b20259d67cec4a1506ca979a8
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-06-08 20:05:50 -0700

    Add files via upload

---
+++ twoString.js
@@ -0,0 +1,26 @@
+
+`https://www.hackerrank.com/challenges/two-strings/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign`
+
+function twoString(s1,s2){
+    s1 = s1.split('');
+    s2 = s2.split('');
+    let result1 =  "YES";
+    let result2 ="NO";
+
+    for(let i=0; i< s1.length; i++){
+        if(s1.includes(s2[i])){
+            return result1;
+        }
+        
+        for(let j=0; j< s2.lenght; j++){
+            if(s2.includes(s1[i])){
+                return result1;
+            }
+        }
+        return result2
+    }
+
+  
+}
+//console.log(twoString('hello', 'world'));
+console.log(twoString('hello', 'world'));
