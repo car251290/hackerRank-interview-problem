736976fc32c794281264c04d908e8586df254c97
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-06-11 13:04:12 -0700

    Add files via upload

--- twoString.js
+++ twoString.js
@@ -2,25 +2,27 @@
 `https://www.hackerrank.com/challenges/two-strings/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign`
 'use strict'
 function twoString(s1,s2){
-    s1 = s1.split('').match(/[a-z]/g);
-    s2 = s2.split('').match(/[a-z]/g);
+  let unique = [...new Set(s1)];
+  let unique2 = [...new Set(s2)];
+ 
+
     let result1 =  "YES";
     let result2 ="NO";
 
-    for(let i= 0; i< s1.length; i++){
-        if(s1.includes(s2[i])){
+    for(let i= 0; i< unique.length; i++){
+        if(s1.includes(unique2[i])){
             return result1;
         }
         
-        for(let j=0; j< s2.lenght; j++){
-            if(s2.includes(s1[i])){
+        for(let j = 0; j< unique2.lenght; j++){
+            if(s2.includes(unique[i])){
                 return result1;
             }
         }
         return result2
-    }
-
-  
+    }  
 }
-//console.log(twoString('hello', 'world'));
+
 console.log(twoString('hello', 'world'));
+console.log(twoString('hi', 'world'));
+
