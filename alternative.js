0333659fa145a25f19b188b602420c906a58b326
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-06-20 15:11:35 -0700

    Add files via upload

--- alternative.js
+++ alternative.js
@@ -5,25 +5,19 @@ function alrternative(s){
 s = 'abaacdabd';
 let max = 0;
 //let unique = [...new Set(s)];
-let unique = s.split('').filter((item, i, ar) => ar.indexOf(item) === i);
-console.log(unique);
-    s = s.replace(/a/g, '4');
-    for(let i=0; i< s.length; i++){
-        if(s[i] ==='e'){
-            s = s.replace('e', '3');
-        } if(s[i] === 'i'){
-            s = s.replace('i','1');
-
-        } if(s[i] === 'o'){
-            s = s.replace('o', '0');
-        } if(s[i] === 'u'){
-            s = s.replace('u', '2');
-        }  if (s[i] ==='b'){
-            s = s.replace('b', '5');
+let unique = s.split('').filter((item, i, arr) => arr.indexOf(item) === i);
+//console.log(unique);    
+    for(let i = 0; i < unique.length; i++){
+        for(let j = i + 1; j < unique.length; j++){
+            let filtered = s.split('').filter((item) => item === unique[i] || item === unique[j]);
+            if(filtered.every((item, i, arr) => item === arr[0])){
+                max = Math.max(max, filtered.length);
+                
+            }
+            
         }
-
     }
-    return s;
+   return max;
 }
 console.log(alrternative('aeiou'));
 console.log(alrternative('hello'));
