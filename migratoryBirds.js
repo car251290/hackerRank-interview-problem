53e59923dfe7d5f57710125c57a66627efedc258
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-08-13 12:31:55 -0600

    Add files via upload

---
+++ migratoryBirds.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function migratoryBirds(arr){
+   let bird = {};
+   let max =0;
+   let maxBird = 0;
+
+   for(let i = 0; i<arr.length; i++){
+    if(bird[arr[i]]){
+        bird[arr[i]]++;
+    } else {
+        bird[arr[i]] =1;
+    }
+
+   }
+   for(let key in bird){
+    if(bird[key ]> max){
+        max = bird[key];
+        maxBird = key;
+    }
+   }
+   return maxBird;
+}
