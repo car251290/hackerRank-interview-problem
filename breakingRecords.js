ecba3118ec9976cecaf8699e65f11ca8c025b25f
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2025-01-20 12:46:31 -0600

    Add files via upload

--- breakingRecords.js
+++ breakingRecords.js
@@ -1,7 +1,18 @@
-`https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem?isFullScreen=true`
-
-'use strict';
-
-function breakingRecords(scores) {
-
+function breakingRecords(scores){
+    let min = scores[0];
+    let max = scores[0];
+    let minCount  = 0;
+    let maxCount = 0;
+for (let i = 0; i<scores.length; i ++){
+    if(scores[i]>max){
+        maxCount++;
+        max = scores[i];
+    } else if(scores[i] < min){
+        minCount++;
+        min = scores[i];
+    } else {
+        continue;
+    }
+}
+return [maxCount, minCount];
 }
