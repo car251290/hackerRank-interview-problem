78263456dea25466bae67a0ef6782e7124be5fc4
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2025-01-27 16:03:03 -0600

    Add files via upload

---
+++ renameFile.js
@@ -0,0 +1,19 @@
+
+function renameFile(newName, oldName) {
+    const n = newName.length;
+    const m = oldName.length;
+    let dp = new Array(m + 1).fill(1);
+
+    for (let i = 1; i <= n; i++) {
+        let dpp = new Array(m + 1).fill(0);
+        for (let j = i; j <= m; j++) {
+            dpp[j] = dpp[j - 1];
+            if (newName[i - 1] === oldName[j - 1]) {
+                dpp[j] += dp[j - 1];
+            }
+        }
+        dp = dpp;
+    }
+
+    return dp[m] % (10**9 + 7);
+}
